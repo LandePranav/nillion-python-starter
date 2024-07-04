193a0ed41a6457800e3e70896cfc4d7d187ae20d
LandePranav <pranavlande1@gmail.com>
2024-07-04 17:25:22 +0000

    encryption and decryption, try 1

--- quickstart/nada_quickstart_programs/src/main.py
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -1,12 +1,31 @@
 from nada_dsl import *
-def nada_main():
+import random
 
+def nada_main():
     party1 = Party(name="Party1")
 
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    new_int = my_int1 + my_int2
+    # function for encryption
+    @nada_fn
+    def encrypt_message(plain: SecretInteger, k: SecretInteger) -> SecretInteger:
+        encrypted = (plain + k) * Integer(3)  
+        return encrypted
 
-    return [Output(new_int, "my_output", party1)]
+    # decryption function
+    @nada_fn
+    def decrypt_message(encrypted: SecretInteger, k: SecretInteger) -> SecretInteger:
+        decrypted = (encrypted / Integer(3)) - k 
+        return decrypted
+
+    #function invocation , then storing returned values
+    encrypted_message = encrypt_message(my_int1, my_int2)
+    decrypted_message = decrypt_message(encrypted_message, my_int2)
+
+    #output formattting
+    output_original = Output(my_int1, "original_message", party1)
+    output_encrypted = Output(encrypted_message, "encrypted_message", party1)
+    output_decrypted = Output(decrypted_message, "decrypted_message", party1)
+
+    return [output_original, output_encrypted, output_decrypted]
